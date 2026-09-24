fa3e4e43efc0d1583e3d49a5d030341e7b7acb4b
catwilo <31023673+catwilo@users.noreply.github.com>
2026-09-23 20:10:02 -0500

    fix(termux): export RISH_APPLICATION_ID for rish

--- dotfiles/.addons-zsh/aliass/termux.zsh
+++ dotfiles/.addons-zsh/aliass/termux.zsh
@@ -3,9 +3,12 @@
 export sd0='/storage/emulated/0'
 # export SD='/storage/...'  # set to your SD card path if present
 
+# Shizuku: rish needs the calling app id to reach the Shizuku service.
+# Without it, rish returns "RISH_APPLICATION_ID is not set".
+export RISH_APPLICATION_ID=com.termux
+
 alias sd='cd /storage/emulated/0'
 alias vT='nvim $HOME/.termux/termux.properties'
 alias vZ='nvim $HOME/.addons-zsh/aliass/shared.zsh'
 alias vz='nvim $HOME/.zshrc'
 alias arch='proot-distro login archlinux --user u --termux-home'
-
